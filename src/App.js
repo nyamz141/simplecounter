843ecdc0c215dc133ceb7aef2c5e5a41b732f46e
nyamz141 <51674315+nyamz141@users.noreply.github.com>
2023-01-19 16:33:28 +1000

    committing project to scm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [counter, setCounter] = useState(0)
+
+    const increment = () => {
+        setCounter(counter + 1)
+    }
+
+    const decrement = () => {
+        setCounter(counter - 1)
+    }
+    return(
+        <div className="container parent-container">
+            <div className="card counter-body shadow p-3 mb-5 bg-body rounded">
+                <div className="card-body">
+                    <div className="row">
+                        <h5 className="card-title count-value mb-5 mt-5">{counter}</h5>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <button className='btn btn-success counter-button mb-2' onClick={increment}>Increment</button>
+                        </div>
+                        <div className="col-sm-6">
+                            <button className='btn btn-warning counter-button mb-2' onClick={decrement}>Decrement</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
